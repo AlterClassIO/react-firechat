7802559683b5a690afc5520ec963270a68e5d09d
Grégory D'Angelo <dangelo.gregory@gmail.com>
2021-01-11 15:23:27 +0100

    feat: create form to send new messages

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import firebase from 'firebase/app';
 import { useFirestoreQuery } from '../hooks';
 
 const Channel = () => {
-  const messages = useFirestoreQuery(
-    firebase.firestore().collection('messages')
-  );
+  const db = firebase.firestore();
+  const messagesRef = db.collection('messages');
+  const messages = useFirestoreQuery(messagesRef);
+
+  const [newMessage, setNewMessage] = useState('');
+
+  const handleOnChange = e => {
+    setNewMessage(e.target.value);
+  };
+
+  const handleOnSubmit = e => {
+    e.preventDefault();
+    // Add new message in Firestore
+    messagesRef.add({
+      text: newMessage,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    // Clear input field
+    setNewMessage('');
+  };
 
   return (
-    <ul>
-      {messages?.map(message => (
-        <li key={message.uid}>{JSON.stringify(message)}</li>
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {messages?.map(message => (
+          <li key={message.uid}>{JSON.stringify(message)}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleOnSubmit}>
+        <input type="text" value={newMessage} onChange={handleOnChange} />
+        <button type="submit">Send</button>
+      </form>
+    </div>
   );
 };
 
