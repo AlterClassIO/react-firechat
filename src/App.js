3e100725cf450f28c3f0b4b1396868624132f462
Grégory D'Angelo <dangelo.gregory@gmail.com>
2021-01-11 11:54:09 +0100

    feat: initialize Firebase app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
+import firebase from 'firebase/app';
+// Firebase products for authentication and database
+import 'firebase/auth';
+import 'firebase/firestore';
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_AUTH_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+});
+
 function App() {
   return <div className="App"></div>;
 }
